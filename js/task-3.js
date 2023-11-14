9980345426384b0617ef67868873f617d66c38b4
AHetman <101942183+AHetman@users.noreply.github.com>
2023-11-14 23:59:41 +0200

    hw-1-1

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,5 @@
 function getElementWidth(content, padding, border) {
-  x = parseFloat(content);
-  y = parseFloat(padding);
-  z = parseFloat(border);
-  const sizing = x + y * 2 + z * 2;
-  return `${sizing}`;
+  return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2;
 }
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
